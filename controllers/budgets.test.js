7d223654e63935fbe8e51cf7b70f17af24e950b3
Tom Kester <kes19002@byui.edu>
2025-12-05 12:05:44 -0700

    added last two tests

--- controllers/budgets.test.js
+++ controllers/budgets.test.js
@@ -41,6 +41,8 @@ describe("Test Budget Routes", function () {
       color: "red",
     });
     await mockCategory.save();
+
+    mockBudget = {};
   });
 
   //runs after all tests in the describe block
@@ -76,24 +78,83 @@ describe("Test Budget Routes", function () {
       .post("/")
       .set("Authorization", "Bearer " + token)
       .send(data);
-      
-      if (Object.hasOwn(res.body, "error")) {
-        console.log(res.body);
-      }
-      //expect json and 201 to come back. Then check for a few fields in the json
+
+    if (Object.hasOwn(res.body, "error")) {
+      console.log(res.body);
+    }
+    //expect json and 201 to come back. Then check for a few fields in the json
     expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
     expect(res.statusCode).toBe(201);
     expect(res.body).toHaveProperty("userId");
     expect(res.body).toHaveProperty("categoryId");
     expect(res.body).toHaveProperty("createdAt");
+    mockBudget = res.body;
   });
 
-
   // test the get /:id route
+  test("responds to get /:id", async () => {
+    const res = await request(app)
+      .get("/" + mockBudget._id)
+      .set("Authorization", "Bearer " + token);
+    if (Object.hasOwn(res.body, "error")) {
+      console.log(res.body);
+    }
+    //excpect json and 200 to come back
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty("userId");
+    expect(res.body).toHaveProperty("categoryId");
+    expect(res.body).toHaveProperty("createdAt");
+  });
 
   // test the put /:id route
+  test("responds to put /:id", async () => {
+    data = {
+      userId: mockUser._id,
+      categoryId: mockCategory._id,
+      month: "2025-02",
+      limit: 2,
+    };
+
+    const res = await request(app)
+      .put("/" + mockBudget._id)
+      .set("Authorization", "Bearer " + token)
+      .send(data);
+
+    if (Object.hasOwn(res.body, "error")) {
+      console.log(res.body);
+    }
+    //expect json and 201 to come back. Then check for a few fields in the json
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty("userId");
+    expect(res.body).toHaveProperty("categoryId");
+    expect(res.body).toHaveProperty("createdAt");
+    expect(res.body.limit).toBe(2);
+    expect(res.body.month).toBe("2025-02");
+    expect(JSON.stringify(res.body.userId)).toEqual(
+      JSON.stringify(mockUser._id)
+    );
+    expect(JSON.stringify(res.body.categoryId)).toEqual(
+      JSON.stringify(mockCategory._id)
+    );
+    mockBudget = res.body;
+  });
 
   // test the delete /:id route
 
+  test("responds to delete /:id", async () => {
+    const res = await request(app)
+      .delete("/" + mockBudget._id)
+      .set("Authorization", "Bearer " + token);
+    if (Object.hasOwn(res.body, "error")) {
+      console.log(res.body);
+    }
 
+    //excpect json and 200 to come back
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty("message");
+    expect(res.body.message).toBe("Budget deleted successfully");
+  });
 });
